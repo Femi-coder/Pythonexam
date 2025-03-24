861b1777001e07739e98ec7105df5dc14f93e4a7
Femi-coder <B00152778@mytudublin.ie>
2025-03-24 14:50:34 +0000

    #A2 created

---
+++ PartA.py
@@ -0,0 +1,8 @@
+#A2
+class House:
+    def __init__(self, house_number, street, area, number_of_beds, price):
+        self.house_number = house_number  
+        self.street = street
+        self.area = area
+        self.number_of_beds = number_of_beds
+        self.price = price 
