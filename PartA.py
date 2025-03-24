ad3742c0f9d779882bab9a17a670c01bdf52caff
Femi-coder <B00152778@mytudublin.ie>
2025-03-24 14:59:33 +0000

    #A4 method to update attributes done

--- PartA.py
+++ PartA.py
@@ -10,3 +10,34 @@ class House:
 #A3 Mtehod to print atributes
 def display_info(self):
     print(f"House Info No: {self.house_number}, Street: {self.street}, Area: {self.area}, Beds: {self.number_of_beds}, Price: €{self.price}")
+    
+ #A4 Method to update atributes
+def update_house_number(self, new_number):
+     if isinstance(new_number, int):
+         self.house_number = new_number
+     else:
+        print("housenumber must be an integer.")
+        
+def update_street(self, new_street):
+        if isinstance(new_street, str):
+            self.street = new_street
+        else:
+            print("street must be a string.")
+            
+def update_area(self, new_area):
+    if isinstance(new_area, str):
+     self.area = new_area
+    else:
+         print("area must be a string.")
+        
+def update_number_of_beds(self, new_beds):
+    if isinstance(new_beds, int):
+        self.number_of_beds = new_beds
+    else:
+        print("number_of_beds must be an integer.")
+        
+def update_price(self, new_price):
+    if isinstance(new_price, (int, float)):
+        self.price = new_price
+    else:
+        print("price must be a number.")
