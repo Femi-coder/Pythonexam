c59fc99b185e0a154137e31ca7773622eac84838
Femi-coder <B00152778@mytudublin.ie>
2025-03-24 15:18:02 +0000

    B2 done

---
+++ PartB.py
@@ -0,0 +1,14 @@
+import unittest
+from PartA import House, RentalHouse
+
+class TestHouseClasses(unittest.TestCase):
+    
+    def setUp(self):
+        self.house1 = House(2,"Banana Ireland","Dublin 1", 3, 400000)
+        self.rental1 = RentalHouse(70, "Castletown", "Dublin 10", 5, 400000, 1400, True)
+        
+#B2 Testing if object is in instance of class
+def test_is_instance(self):
+    self.assertIsInstance(self.house1, House)
+    self.assertIsInstance(self.rental1, RentalHouse)
+    self.assertIsInstance(self.rental1, House)
